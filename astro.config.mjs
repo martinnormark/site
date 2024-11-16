d040cc04fad0222bc8cd002925b6b758e4e66034
Martin H. Normark <m@martinnormark.com>
2024-11-16 20:53:57 +0100

    Add main structure, get MDX running

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,43 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-
-import cloudflare from '@astrojs/cloudflare';
+import { defineConfig } from "astro/config";
+import cloudflare from "@astrojs/cloudflare";
+import tailwind from "@astrojs/tailwind";
+import mdx from "@astrojs/mdx";
+import rehypePrettyCode from "rehype-pretty-code";
+import rehypeSlug from "rehype-slug";
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
+  site: "https://www.martinnormark.com",
+  output: "server",
+
   adapter: cloudflare({
     platformProxy: {
-      enabled: true
-    }
-  })
+      enabled: true,
+    },
+  }),
+
+  integrations: [
+    tailwind(),
+    mdx({
+      syntaxHighlight: false,
+      rehypePlugins: [
+        /**
+         * Adds ids to headings
+         */
+        rehypeSlug,
+        [
+          /**
+           * Enhances code blocks with syntax highlighting, line numbers,
+           * titles, and allows highlighting specific lines and words
+           */
+
+          rehypePrettyCode,
+          {
+            theme: "github-dark",
+          },
+        ],
+      ],
+    }),
+  ],
 });
